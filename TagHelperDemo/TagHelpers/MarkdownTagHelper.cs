bda46af3a7625bb408b8fc3de81636b3fb514432
Anuraj <anuraj.p@msn.com>
2017-12-12 06:24:23 +0530

    Markdown tag helper added

---
+++ TagHelperDemo/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using HeyRed.MarkdownSharp;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace TagHelperDemo.TagHelpers
+{
+    [HtmlTargetElement("markdown")]
+    public class MarkdownTagHelper : TagHelper
+    {
+        [HtmlAttributeName("text")]
+        public string Text { get; set; }
+
+        [HtmlAttributeName("source")]
+        public ModelExpression Source { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var markdownTransformer = new Markdown();
+            if (Source != null)
+            {
+                Text = Source.Model.ToString();
+            }
+
+            var result = markdownTransformer.Transform(Text);
+            output.TagName = "div";
+            output.Content.SetHtmlContent(result);
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}

8f2fbfc402ea18c1d6688800e71696017ce6b8e8
Anuraj <anuraj.p@msn.com>
2021-04-03 06:51:20 +0530

    File Content type validation implemented.

--- AngularFileUploadDemo/Controllers/FileManagementController.cs
+++ AngularFileUploadDemo/Controllers/FileManagementController.cs
@@ -7,7 +7,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos.Table;
 
+using Myrmec;
+
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace AngularFileUploadDemo.Controllers
@@ -20,21 +24,45 @@ namespace AngularFileUploadDemo.Controllers
         [Route("upload")]
         public async Task<IActionResult> Upload()
         {
+            var sniffer = new Sniffer();
+            var supportedFiles = new List<Record>
+    {
+        new Record("doc xls ppt msg", "D0 CF 11 E0 A1 B1 1A E1"),
+        new Record("jpg,jpeg", "ff,d8,ff,db"),
+        new Record("png", "89,50,4e,47,0d,0a,1a,0a"),
+        new Record("pdf", "25 50 44 46"),
+        new Record("gif", "47 49 46 38 39 61")
+    };
+            sniffer.Populate(supportedFiles);
+
             var formCollection = await Request.ReadFormAsync();
             var files = formCollection.Files;
             foreach (var file in files)
             {
-                var blobContainerClient = new BlobContainerClient("UseDevelopmentStorage=true", "images");
-                blobContainerClient.CreateIfNotExists();
-                var containerClient = blobContainerClient.GetBlobClient(file.FileName);
-                var blobHttpHeader = new BlobHttpHeaders
+                byte[] fileHead = ReadFileHead(file);
+                var results = sniffer.Match(fileHead);
+                if (results.Count > 0)
                 {
-                    ContentType = file.ContentType
-                };
-                await containerClient.UploadAsync(file.OpenReadStream(), blobHttpHeader);
+                    var blobContainerClient = new BlobContainerClient("UseDevelopmentStorage=true", "images");
+                    blobContainerClient.CreateIfNotExists();
+                    var containerClient = blobContainerClient.GetBlobClient(file.FileName);
+                    var blobHttpHeader = new BlobHttpHeaders
+                    {
+                        ContentType = file.ContentType
+                    };
+                    await containerClient.UploadAsync(file.OpenReadStream(), blobHttpHeader);
+                }
             }
 
             return Ok();
+        }
+
+        private static byte[] ReadFileHead(IFormFile file)
+        {
+            using var fs = new BinaryReader(file.OpenReadStream());
+            var bytes = new byte[20];
+            fs.Read(bytes, 0, 20);
+            return bytes;
         }
 
         [HttpPost]

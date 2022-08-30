3195c351bd9c7663ce2ee1c865a59ea4a8479f07
Anuraj <anuraj.p@msn.com>
2022-08-30 11:27:01 +0530

    Custom Binding Minimal API

---
+++ CustomBindingMinimalApi/Program.cs
@@ -0,0 +1,67 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapPost("/upload", (Photo photo) =>
+{
+    app.Logger.LogInformation($"File uploaded with size {photo.File?.Length}");
+
+    return Results.Ok();
+}).Accepts<Photo>("multipart/form-data");
+
+app.Run();
+
+record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+class Photo
+{
+    public IFormFile? File { get; set; }
+    public static async ValueTask<Photo?> BindAsync(HttpContext context,
+                                                   ParameterInfo parameter)
+    {
+        var form = await context.Request.ReadFormAsync();
+        var file = form.Files["file"];
+        return new Photo
+        {
+            File = file
+        };
+    }
+}
